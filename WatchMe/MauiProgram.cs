d811f51197eb74cef16a8072c80a9199b69a09b5
Andrew Moore <drewzisa@gmail.com>
2024-11-18 23:05:46 -0800

    Updated to upload to azure, once storage container connection string was configured

--- WatchMe/MauiProgram.cs
+++ WatchMe/MauiProgram.cs
@@ -1,6 +1,10 @@
 ﻿using Camera.MAUI;
+using CommunityToolkit.Maui;
 using Microsoft.Extensions.Logging;
 using Plugin.Maui.ScreenRecording;
+using WatchMe.Pages;
+using WatchMe.Persistance;
+using WatchMe.Persistance.Implementations;
 using WatchMe.Repository;
 
 namespace WatchMe
@@ -12,6 +16,7 @@ namespace WatchMe
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseMauiCommunityToolkit()
                 .UseMauiCameraView()
                 .UseScreenRecording()
                 .ConfigureFonts(fonts =>
@@ -21,7 +26,9 @@ namespace WatchMe
                 });
 
             builder.Services.AddTransient<SplitCameraRecordingPage>();
+            builder.Services.AddTransient<SettingsPage>();
             builder.Services.AddTransient<IFileSystemServiceFactory, FileSystemServiceFactory>();
+            builder.Services.AddTransient<ICloudProviderService, CloudProviderService>();
             builder.Services.AddSingleton<MainPage>();
 
 #if DEBUG
---
+++ WatchMe/Persistance/CloudProviderService.cs
@@ -0,0 +1,44 @@
+﻿using Azure.Storage.Blobs;
+
+namespace WatchMe.Persistance
+{
+    public interface ICloudProviderService
+    {
+        Task UploadContentToCloud(Stream fileStream, string contentName);
+        Task<string> GetAzureConnectionString();
+
+        Task SetAzureConnectionString(string connstr);
+    }
+
+    public class CloudProviderService : ICloudProviderService
+    {
+        private const string AZURESTORAGECONTAINERCONNECTIONSTRINGKEY = "azure_storagecontainer_connectionstring";
+        public async Task UploadContentToCloud(Stream fileStream, string contentName)
+        {
+            //Right now, we only have Azure configured.
+
+            string storageContainerConnectionString = await SecureStorage.Default.GetAsync(AZURESTORAGECONTAINERCONNECTIONSTRINGKEY);
+
+            if (storageContainerConnectionString == null)
+            {
+                throw new Exception($"unable to gather {AZURESTORAGECONTAINERCONNECTIONSTRINGKEY} from secure storage");
+            }
+
+            var blobServiceClient = new BlobServiceClient(storageContainerConnectionString);
+
+            string containerName = "watchmefileuploadcontainer";
+
+            var containerClient = blobServiceClient.GetBlobContainerClient(containerName);
+
+
+            BlobClient blobClient = containerClient.GetBlobClient(contentName);
+            var result = await blobClient.UploadAsync(fileStream, true);
+        }
+
+        public async Task<string> GetAzureConnectionString() =>
+            await SecureStorage.Default.GetAsync(AZURESTORAGECONTAINERCONNECTIONSTRINGKEY);
+
+        public async Task SetAzureConnectionString(string connstr) =>
+            await SecureStorage.Default.SetAsync(AZURESTORAGECONTAINERCONNECTIONSTRINGKEY, connstr);
+    }
+}
