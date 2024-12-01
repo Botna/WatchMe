54e07be8e8b67a14f876e7b969ecfd9f8a370de4
Andrew Moore <drewzisa@gmail.com>
2024-11-30 23:45:46 -0800

    got basic UI tests working

---
+++ WatchMe.UITests.Shared/MainPageTests.cs
@@ -0,0 +1,26 @@
+﻿namespace UITests;
+
+// This is an example of tests that do not need anything platform specific
+public class MainPageTests : BaseTest
+{
+    [Test]
+    public void AppLaunches()
+    {
+        App.GetScreenshot().SaveAsFile($"{nameof(AppLaunches)}.png");
+    }
+
+    [Test]
+    public void ClickCounterTest()
+    {
+        // Arrange
+        // Find elements with the value of the AutomationId property
+        var element = FindUIElement("Main_SettingsBtn");
+
+        // Act
+        element.Click();
+        Task.Delay(500).Wait(); // Wait for the click to register and show up on the screenshot
+
+        // Assert
+        App.GetScreenshot().SaveAsFile($"{nameof(ClickCounterTest)}.png");
+    }
+}
--- WatchMe/Platforms/Android/MainActivity.cs
+++ WatchMe/Platforms/Android/MainActivity.cs
@@ -1,10 +1,11 @@
 ﻿using Android.App;
 using Android.Content.PM;
-using Android.OS;
+using Android.Runtime;
 
 namespace WatchMe
 {
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    [Register("com.dbstudios.watchme.MainActivity")]
     public class MainActivity : MauiAppCompatActivity
     {
     }
